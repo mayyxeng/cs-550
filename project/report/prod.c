4759f2c0c5ec51bf6b0dcf33df6181c2af0ec120
rishabh246 <rishabh246@gmail.com>
2021-11-13 23:27:18 +0100

    Fix wrap-around in code to enable shrinking

--- project/report/prod.c
+++ project/report/prod.c
@@ -5,7 +5,8 @@ assume sub_len = $\hat{\texttt{sub\_len}}$;
 i = 0; $\hat{\texttt{i}}$ = 0; j = 0; $\hat{\texttt{j}}$ = 0;
 assert (i < len) = ($\hat{\texttt{i}}$ < $\hat{\texttt{len}}$); // trivial
 while (i < len) do:
-    assert ((i $\geq$ l_idx) && (i < l_idx + sub_len)) = (($\hat{\texttt{i}}$ $\geq$ _l_idx) && ($\hat{\texttt{i}}$ < _l_idx + $\hat{\texttt{sub\_len}}$) // fails;
+    assert ((i $\geq$ l_idx) && (i < l_idx + sub_len))
+    		= (($\hat{\texttt{i}}$ $\geq$ _l_idx) && ($\hat{\texttt{i}}$ < _l_idx + $\hat{\texttt{sub\_len}}$) // fails;
     if ((i $\geq$ l_idx) && (i < l_idx + sub_len)) then
         assert i = $\hat{\texttt{i}}$ && j = $\hat{\texttt{j}}$; // trivial
         out[j] = in[i]; $\hat{\texttt{out}}$[$\hat{\texttt{j}}$] = $\hat{\texttt{in}}$[_i];
